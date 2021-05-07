0038a0ad8f1835de6775a34bedcb67566c175657
Mohammad Haque <MHaque@suitsupply.com>
2021-05-08 01:54:57 +0200

    added sample func with appsettings

---
+++ src/FunctionApp1/Function1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public class Function1
+    {
+        private readonly EnvironmentInfo _environmentInfo;
+        private readonly AuthenticationSettings _authenticationl;
+        public Function1(IOptions<AuthenticationSettings> options, EnvironmentInfo environment)
+        {
+            _authenticationl = options.Value;
+            _environmentInfo = environment;
+        }
+
+        [FunctionName("Function1")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+           
+            return new OkObjectResult($"It works with ENV: {_environmentInfo.Environment} : {JsonConvert.SerializeObject(_authenticationl)})");
+        }
+    }
+}
---
+++ src/FunctionApp1/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FunctionApp1;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FunctionApp1
+{
+    public class Startup : FunctionsStartup
+    {
+        private IConfiguration _configuration;
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<EnvironmentInfo>(new EnvironmentInfo
+                {Environment = builder.GetContext().EnvironmentName});
+            builder.Services.Configure<AuthenticationSettings>(_configuration.GetSection("AuthenticationSettings"));
+
+        }
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var context = builder.GetContext();
+            var environment = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT");
+            builder.ConfigurationBuilder
+                .SetBasePath(context.ApplicationRootPath)
+                .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            _configuration = builder.ConfigurationBuilder.Build();
+        }
+    }
+
+    public class AuthenticationSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public Dictionary<string, string> HostAndResourceMappings { get; set; }
+        public string AuthUrl { get; set; }
+    }
+
+    public class EnvironmentInfo
+    {
+        public string Environment { get; set; }
+
+    }
+}
